550b4f7eb78f48771553cfa211b5005c003235c4
liladino <pampadaniel10@gmail.com>
2024-12-08 01:22:06 +0100

    refactoring for the start of the new legalmoves generation

--- hedgineUCI/main.c
+++ hedgineUCI/main.c
@@ -12,9 +12,7 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 	#endif
-	
-	initializeAll();
-	
+		
 	for (int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
     }
@@ -24,16 +22,16 @@ int main(int argc, char* argv[]){
 		}
 		else{
 			printf("Tests failed.\n");
-			freeTransTable();
-			fclose(debugOutput);
+			goto endEverything;
 			return 0;
 		}
 	}
 	if (argc >= 3 && strncmp(argv[2], "quit", 4) == 0){
-		freeTransTable();
-		fclose(debugOutput);
+		goto endEverything;
 		return 0;
 	}
+	
+	initializeAll();
 	
 	bool tomove = white;
 	int movenum = 1, fmv = 0; //fifty-move rule
@@ -42,9 +40,11 @@ int main(int argc, char* argv[]){
 
 	//main uci loop
 	UCIloop(&board, &tomove, &fmv, &movenum);
-			
+	
+	
 	freeTransTable();
 	
+	endEverything:
 	#ifdef DEBUG
 	fprintf(debugOutput, "runtime: %ld\n", getTime_ms() - runStartTime);
 	fclose(debugOutput);
--- hedgineUCI/tests.c
+++ hedgineUCI/tests.c
@@ -31,6 +31,7 @@ bool makePerfTestsAssert(){
 	bool tomove;
 	printf("\nPosition 1\n");
 	setboardFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
 	int startposValues[] = {1, 20, 400, 8902, 197281};
 	for (int i = 0; i < 5; i++){
 		int allmovecount = perfTest(bboard, tomove, i);
@@ -38,17 +39,23 @@ bool makePerfTestsAssert(){
 		if (allmovecount != startposValues[i]) return false;
 	}
 	
-	printf("\nPosition 2\n");
-	//~ setboardFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", &bboard, &tomove, &temp, &temp);
-	//~ int pos2Values[] = {1, 48, 2039, 97862, 4085603};
-	//~ for (int i = 0; i < 5; i++){
-		//~ int allmovecount = perfTest(bboard, tomove, i);
-		//~ printf("%d ply:\nLeaves:\t%d\nExp:\t%d\n\n", i+1, allmovecount, pos2Values[i]);
-		//~ if (allmovecount != pos2Values[i]) return false;
-	//~ }
+	//skip the test for now
+	goto test3;
 	
+	printf("\nPosition 2\n");
+	setboardFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
+	int pos2Values[] = {1, 48, 2039, 97862, 4085603};
+	for (int i = 0; i < 5; i++){
+		int allmovecount = perfTest(bboard, tomove, i);
+		printf("%d ply:\nLeaves:\t%d\nExp:\t%d\n\n", i+1, allmovecount, pos2Values[i]);
+		if (allmovecount != pos2Values[i]) return false;
+	}
+	
+	test3:
 	printf("\nPosition 3\n");
 	setboardFEN("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
 	int pos3Values[] = {1, 14, 191, 2812, 43238};
 	for (int i = 0; i < 5; i++){
 		int allmovecount = perfTest(bboard, tomove, i);
@@ -58,6 +65,7 @@ bool makePerfTestsAssert(){
 	
 	printf("\nPosition 4\n");
 	setboardFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
 	int pos4Values[] = {1, 6, 264, 9467, 422333};
 	for (int i = 0; i < 5; i++){
 		int allmovecount = perfTest(bboard, tomove, i);
@@ -67,6 +75,7 @@ bool makePerfTestsAssert(){
 	
 	printf("\nPosition 4b\n");
 	setboardFEN("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1 ", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
 	for (int i = 0; i < 5; i++){
 		int allmovecount = perfTest(bboard, tomove, i);
 		printf("%d ply:\nLeaves:\t%d\nExp:\t%d\n\n", i+1, allmovecount, pos4Values[i]);
@@ -75,6 +84,7 @@ bool makePerfTestsAssert(){
 	
 	printf("\nPosition 5\n");
 	setboardFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
 	int pos5Values[] = {1, 44, 1486, 62379, 2103487};
 	for (int i = 0; i < 5; i++){
 		int allmovecount = perfTest(bboard, tomove, i);
@@ -84,6 +94,7 @@ bool makePerfTestsAssert(){
 	
 	printf("\nPosition 6\n");
 	setboardFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", &bboard, &tomove, &temp, &temp);
+	printBitBoard2d(stdout, bboard);
 	int pos6Values[] = {1, 46, 2079, 89890};
 	for (int i = 0; i < 4; i++){
 		int allmovecount = perfTest(bboard, tomove, i);
